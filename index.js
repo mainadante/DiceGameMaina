27d7c0f77f1275b9a371ceaca37a5e632eed34bd
mainadante <146443665+mainadante@users.noreply.github.com>
2023-11-20 17:47:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
 }
 else if (randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "🚩Player 2 Wins!";
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
 }
 else {
-    document.querySelector("h1").innerHTML = "Draw!";
+    document.querySelector("h1").innerHTML = "🚩Draw!🚩";
 }
